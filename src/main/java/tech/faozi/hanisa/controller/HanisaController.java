3e45f8eabf7236cad6f739e17e0d904ce12a5b44
Aziz Faozi <admin@labseni.com>
2020-11-15 02:28:53 +0800

    mantap fixing untuk versi portofolio

--- src/main/java/tech/faozi/hanisa/controller/HanisaController.java
+++ src/main/java/tech/faozi/hanisa/controller/HanisaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("hanisa")
+@RequestMapping("portofolio")
 public class HanisaController {
     @Autowired
     HanisaService hanisaService;
