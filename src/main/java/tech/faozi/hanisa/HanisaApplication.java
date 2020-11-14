9bc34ac41b36c30c899876c03ada7c5b124e7588
Aziz Amerul Faozi <admin@labseni.com>
2020-11-14 14:52:47 +0700

    mantap

---
+++ src/main/java/tech/faozi/hanisa/HanisaApplication.java
@@ -0,0 +1,13 @@
+package tech.faozi.hanisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanisaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/faozi/hanisa/HanisaApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.faozi.hanisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
